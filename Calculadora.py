2738df41852902590e312a02b28b4d59c460ca50
Vinicius Santana <contactuvinicius@gmail.com>
2022-10-13 15:48:09 -0300

    Calculadora 1.0

--- Calculadora.py
+++ Calculadora.py
@@ -4,35 +4,42 @@ div = 39 * '=' + '\n'
 
 # home
 
-print('\n' + div)
-print('    Bem vindo à calculadora geral!\n    Escolha uma das opções abaixo:')
+i = 0
 
-print("""
+while i == 0:
+        print('\n' + div)
+        print('    Bem vindo à calculadora geral!\n    Escolha uma das opções abaixo:')
+
+        print("""
 
         1 - Soma
         2 - Multiplicação
         3 - Divisão
         4 - Potência
 
-""")
-
-i = 0
-i = int(input('        > '))
-
-print('\n' + div)
-
-        # laço lista
-while i == 1:
-        print('Você escolheu a opção de soma!')
-        print('Digite a quantidade de números\n')
-        repetsoma = int(input('       > '))
+        """)
         
-        print('\n\nDigite ')
-        while i < repetsoma:
+        i = int(input('        > '))
+
+        print('\n' + div)
+
+                # laço lista
+        while i == 1:
+                print('        Você escolheu a opção de soma!')
+                print('        Digite a quantidade de números que deseja somar:\n')
+                repetsoma = int(input('       > '))
                 
+                print('\n\nDigite os números a serem somados:\n')
+                while i <= repetsoma:
+                        numsoma = float(input('       > '))
+                        numsoma = numsoma + numsoma
+                        i = i + 1
+                        
+                print('\nO resultado da soma é', numsoma)
+                input('\n>>Aperte enter para continuar<<')
         
-        i = i + 1
-        # end laço lista
+        i = 0
+                # end laço lista
 
 # end home
 
