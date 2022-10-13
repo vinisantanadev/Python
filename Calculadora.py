ec671c46c18b80af87e43da2999a8570ba0e27de
Vinicius Santana <contactuvinicius@gmail.com>
2022-10-13 13:50:56 -0300

    Calculadora alpha

---
+++ Calculadora.py
@@ -0,0 +1,28 @@
+
+# config das divisões:
+div = 39 * '=' + '\n'
+
+# home
+
+print('\n' + div)
+print('    Bem vindo à calculadora geral!\n    Escolha uma das opções abaixo:')
+
+print("""
+
+        1 - Soma
+        2 - Multiplicação
+        3 - Divisão
+        4 - Potência
+
+""")
+
+a, b = 0, 1
+
+while a < 10:
+        print
+
+print(div)
+
+# end home
+
+
