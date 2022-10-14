21d628bdbbc3b631e80b9c5215f807f088a89ca1
Vinicius Santana <82420721+vinisantanadev@users.noreply.github.com>
2022-10-14 14:48:46 -0300

    Calculadora 2.1

--- Calculadora.py
+++ Calculadora.py
@@ -15,20 +15,21 @@ operador = '='
 print('\n' + div)
 
 print('Bem vindo à calculadora')
-print('Você sempre pode ver um tutorial de uso digitando 0!\n')
+print('Você sempre pode ver um tutorial de uso digitando help!\n')
 
-while operador == '=' or operador == '+' or operador == '-' or operador == '*':
-        
-        #numero incial do usuário
+# atualizar conforme as opções aumentam
+while operador != '.' or operador != 'exit':
+
+        # numero incial do usuário
         if operador == '=':
                 calculo = float(input(userinput + 'num> '))
         
         # caso o usuário queira ver o tutorial
-        if calculo == 0:
+        if operador == 'h' or operador == 'help':
                 print('\nComece digitando um número\nEm seguida digite o operador desejado\nComo * ou +, por exemplo\nDigite = para terminar a conta e exibir o resultado\nE o app faz o resto!\nDigite . para sair do app\n\n' + div)
-                
+
                 calculo = float(input(userinput + 'num> '))
-                
+
         # input que vai decidir a conta do usuário 
         if operador == '=':
                 operador = input(userinput + 'ope> ')
@@ -58,17 +59,37 @@ while operador == '=' or operador == '+' or operador == '-' or operador == '*':
                 calculo = calculo ** num
                 operador = (input(userinput + 'ope> '))
 
+        # caso o usuário digite /
+        while operador == '/':
+                num = float(input(userinput + 'num> '))
+                calculo = calculo / num
+                operador = (input(userinput + 'ope> '))
+                
+        # caso o usuário digite %
+        while operador == '%':
+                calculo = calculo % 2 # par ou impar
+                if calculo == 0:
+                        print('\n        Este número é par!')
+
+                else:
+                        print('\n        Este número é impar!')
+
+
+                print('        -------------------\n')
+
+                operador = (input(userinput + 'ope> '))
+
         # printa o resultado caso o usuário digite =
-        print('\n' + 7 * ' ', calculo)
-        print('        -------------\n')
-        
-        # continua a conta
-        operador = (input(userinput + 'ope> '))
+        while operador == '=':
+                print('\n' + 7 * ' ', calculo)
+                print('        -------------------\n')
+                
+                # continua a conta
+                operador = (input(userinput + 'ope> '))
         
         # terminar o programa caso o usuário digite .
         if operador == '.' or operador == 'exit':
-                print('\nFechando app')
-                
+                break
 print('\n' + div)
 
 # end laço do app
